5e68bb374ae8e06f4f8bda241b038be368c6d5d5
Ethan Barlow <ren.o.amer.2016@hotmail.com>
2018-04-14 02:14:44 -0400

    Added GUI elements, mainly the output boxes.

--- src/javaapplication3/Main.java
+++ src/javaapplication3/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
 
     private VBox vbMainContain;
     private HBox hbMethodChoice;
+    private HBox shiftContainer;
+    private HBox repeatContainer;
 
     private ToggleButton tbtnEncode;
     private ToggleButton tbtnDecode;
@@ -22,7 +24,10 @@ public class Main extends Application {
     private ToggleGroup tgMethodChoice;
 
     private TextArea input;
-    private TextArea output;
+    private TextArea[] output;
+
+    private TextField shiftField;
+    private TextField repeatKeyField;
 
     private CheckBox[] ciphers;
 
@@ -38,7 +43,7 @@ public class Main extends Application {
         tgMethodChoice.getToggles().add(tbtnEncode);
         tgMethodChoice.getToggles().add(tbtnDecode);
         tgMethodChoice.getToggles().add(tbtnBreak);
-        //tgMethodChoice.getToggles().get(0).setSelected(true); //preselection -- starts with this button toggled
+        tgMethodChoice.getToggles().get(0).setSelected(true); //preselection -- starts with this button toggled
         vbMainContain = new VBox();
         hbMethodChoice = new HBox();
 
@@ -49,20 +54,61 @@ public class Main extends Application {
 
         input = new TextArea();
         input.setPromptText("Your message here...");
-        output = new TextArea("Output will go here");
-        output.setEditable(false);
-        output.setFocusTraversable(false);
 
-       process = new Button("Process");
+        output = new TextArea[NUM_CIPHERS];
+        for (int i = 0; i < NUM_CIPHERS; i++) {
+            output[i] = new TextArea();
+            output[i].setPromptText("Output will go here...");
+            output[i].setWrapText(true);
+            output[i].setEditable(false);
+            output[i].setDisable(true);
+        }
 
+        shiftField = new TextField();
+        repeatKeyField = new TextField();
+
+        shiftContainer = new HBox();
+        repeatContainer = new HBox();
+
+        shiftContainer.getChildren().add(new Label("Shift: "));
+        shiftContainer.getChildren().add(shiftField);
+        shiftContainer.setDisable(true);
+        repeatContainer.getChildren().add(new Label("Keyword: "));
+        repeatContainer.getChildren().add(repeatKeyField);
+        repeatContainer.setDisable(true);
+
+
+
+        //setup of the button actions
+        ciphers[0].setOnAction(event -> {
+            output[0].setDisable(!output[0].isDisabled());//toggles whether or not the output boxes will be active
+        });
+        ciphers[1].setOnAction(event -> {
+            output[1].setDisable(!output[1].isDisabled());//toggles whether or not the output boxes will be active
+            shiftContainer.setDisable(!shiftContainer.isDisabled());
+        });
+        ciphers[2].setOnAction(event -> {
+            output[2].setDisable(!output[2].isDisabled());//toggles whether or not the output boxes will be active
+            repeatContainer.setDisable(!repeatContainer.isDisabled());
+        });
 
         hbMethodChoice.getChildren().addAll(tbtnEncode, tbtnDecode, tbtnBreak);
         vbMainContain.getChildren().add(hbMethodChoice);
-        vbMainContain.getChildren().addAll(ciphers);
-        vbMainContain.getChildren().add(input);
-        vbMainContain.getChildren().add(output);
-        vbMainContain.getChildren().add(process);
+        for (int i = 0; i < ciphers.length; i++) {
+            vbMainContain.getChildren().add(ciphers[i]);
+            vbMainContain.getChildren().add(output[i]);
+            if(i==1)
+                vbMainContain.getChildren().add(shiftContainer);
+            if(i==2)
+                vbMainContain.getChildren().add(repeatContainer);
+        }
 
+        //vbMainContain.getChildren().addAll(ciphers);
+        vbMainContain.getChildren().add(input);
+
+        process = new Button("Process");
+        //vbMainContain.getChildren().add(output);
+        vbMainContain.getChildren().add(process);
 
 
         Scene root = new Scene(vbMainContain, 300, 500);
